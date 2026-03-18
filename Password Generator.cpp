ee260e9f4c8c70bc4523552992ce43bc558cffcc
gouravsng <gk1313340@gmail.com>
2026-03-18 16:03:40 +0530

    Add files via upload
    This is a simple Password Generator program written in C++. It generates a random password of user-defined length using a mix of uppercase letters, lowercase letters, and numbers.
    
    The program uses random number generation (rand() and srand(time(0))) to ensure that each password is unique every time the program runs.

---
+++ Password Generator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+//function start 
+
+string getPassword(int len){
+    string Password = "";
+    string characters = "aquickbrownfoxjumpoverthelazydogAQUICKBROWNFOXJUMPOVERTHELAZYDOG0123456789";
+    int charSize = characters.size();
+    srand(time(0));
+    int randomIndex;
+    for(int i = 0 ; i < len; i++){
+        randomIndex = rand() % charSize;
+        Password = Password + characters[randomIndex];
+    }
+    return Password;
+}
+
+int main(){
+    int len;
+    cout << "Enter the Length of number : ";
+    cin>>len;
+    string password = getPassword(len);
+    cout << "Generated Password :: " << password;
+
+    return 0;
+
+}
